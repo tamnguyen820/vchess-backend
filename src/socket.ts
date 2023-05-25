e36b5799a0a9ee5e2598acfa18c9966dd79c2d36
Tam <tam.nguyen8232@gmail.com>
2023-05-24 21:17:10 -0400

    Socket disconnect

--- src/socket.ts
+++ src/socket.ts
@@ -2,13 +2,17 @@ import { Server, Socket } from "socket.io";
 
 const EVENTS = {
   connection: "connection",
+  disconnect: "disconnect",
 };
 
 function socket({ io }: { io: Server }) {
   console.log("Sockets enabled");
 
   io.on(EVENTS.connection, (socket: Socket) => {
-    console.log(`A user connected ${socket.id}`);
+    console.log(`A user connected: ${socket.id}`);
+    socket.on(EVENTS.disconnect, () => {
+      console.log(`A user disconnected: ${socket.id}`);
+    });
   });
 }
 
