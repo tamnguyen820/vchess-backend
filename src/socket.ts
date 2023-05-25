952c72e127e8b43eaf27d6a0e3ce6203c9ba9882
Tam <tam.nguyen8232@gmail.com>
2023-05-24 22:34:03 -0400

    Add room logic and move logic in socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,17 +1,52 @@
 import { Server, Socket } from "socket.io";
+import { nanoid } from "nanoid";
 
 const EVENTS = {
+  // Utils
   connection: "connection",
   disconnect: "disconnect",
+  message: "message",
+
+  // Room
+  createRoom: "createRoom",
+  joinRoom: "joinRoom",
+
+  // Game
+  move: "move",
+  drawOffer: "drawOffer",
+  acceptDrawOffer: "acceptDrawOffer",
+  resign: "resign",
 };
 
 function socket({ io }: { io: Server }) {
   console.log("Sockets enabled");
 
   io.on(EVENTS.connection, (socket: Socket) => {
-    console.log(`A user connected: ${socket.id}`);
+    console.log(`A user connected, socket ${socket.id}`);
+
     socket.on(EVENTS.disconnect, () => {
-      console.log(`A user disconnected: ${socket.id}`);
+      console.log(`A user disconnected, socket ${socket.id}`);
+    });
+
+    socket.on(EVENTS.createRoom, () => {
+      const roomId = nanoid();
+      socket.emit(EVENTS.message, `Created room ${roomId}`);
+      socket.join(roomId);
+      socket.emit(EVENTS.message, `Socket ${socket.id} joined room ${roomId}`);
+    });
+
+    socket.on(EVENTS.joinRoom, (roomId: string) => {
+      if (roomId) {
+        socket.join(roomId);
+        socket.broadcast.emit(
+          EVENTS.message,
+          `Socket ${socket.id} joined room ${roomId}`
+        );
+      }
+    });
+
+    socket.on(EVENTS.move, (move: string) => {
+      socket.broadcast.emit(EVENTS.move, move);
     });
   });
 }
